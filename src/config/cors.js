f8fb49cf16a9593785de36f691b29dec1e2dd25c
ClowKhxu <clowkhuux2707@gmail.com>
2025-02-14 18:07:32 +0700

    cap nhat 1

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,11 +1,12 @@
+
 require("dotenv").config();
 
 const configCors = (app) => {
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://thanhdatflix.vercel.app'); // Thay trực tiếp URL frontend
+        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', 'true'); // Phải đặt giá trị kiểu string
+        res.setHeader('Access-Control-Allow-Credentials', true);
 
         if (req.method === 'OPTIONS') {
             return res.sendStatus(200);
